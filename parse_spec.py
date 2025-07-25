ffe7210a27fb5fd8321578e0fc1f56b9abac5980
Abhigyan Gargeshwari <abhigyan.gargeshwari@gmail.com>
2025-07-25 11:59:07 -0700

    updated parse file

--- parse_spec.py
+++ parse_spec.py
@@ -1,13 +1,35 @@
 from prance import ResolvingParser
 
 parser = ResolvingParser("specs/petstore.yaml")
-
-print("spec loaded successfully")
 specs = parser.specification
 
-print("\nEndpoints in the Petstore API:\n")
+print("spec loaded successfully")
 
 for path, methods in specs["paths"].items():
-    print(f"Path: {path}")
-    for method in methods:
-        print(f"  - Method: {method.upper()}")
+    print(f"\n Path: {path}")
+
+    for method, details in methods.items():
+        method_indent = "  |--"
+        sub_indent = "  |   "
+        print(f"{method_indent} Method: {method.upper()}")
+
+        operation_id = details.get("operationId")
+        if operation_id:
+            print(f"{sub_indent}|- Operation ID: {operation_id}")
+
+        request_body = details.get("requestBody")
+        if request_body:
+            print(f"{sub_indent}|- Request Body: {request_body.get('description', '(no description)')}")
+            content = request_body.get("content", {})
+            for content_type in content:
+                print(f"{sub_indent}   |-- Content-type: {content_type}")
+
+        responses = details.get("responses", {})
+        if responses:
+            print(f"{sub_indent}|- Responses:")
+            for status_code, response in responses.items():
+                desc = responses.get("description", "")
+                print(f"{sub_indent}  |-- {status_code}: {desc}")
+                content = response.get("content", {})
+                for content_type in content:
+                    print(f"{sub_indent}     |- Content-type: {content_type}")
